1ff9ec1f46f7d52be6b81ce8847befdbb55b250e
bowwow <jummykimo@gmail.com>
2016-05-20 01:45:24 +0800

    update blog at 2016年 5月20日 周五 01時45分24秒 CST

--- blog/push.sh
+++ blog/push.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ./hugo --theme=grid-side --baseUrl="http://bowwow.tips"
 info="update blog at `date`"
+git commit -am "$info" && git push origin dev
 git --git-dir=$PWD/public/.git --work-tree=$PWD/public commit -am "$info"
 git --git-dir=$PWD/public/.git --work-tree=$PWD/public push origin gh-pages
